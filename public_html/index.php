fd4a595c1a1005e681214b2a4acc5339cdba48a4
zonoise <zonoise@gmail.com>
2011-11-10 07:06:16 +0900

    add Twig setting

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,20 @@ require_once __DIR__.'/silex.phar';
 
 $app = new Silex\Application(); 
 
+//Register Twig Extension
+$app->register(new Silex\Provider\TwigServiceProvider(),array(
+  'twig.path'           => __DIR__ . '/../views' ,
+  'twig.class_path' => __DIR__ .  '/../vender'
+));
+
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
 
-$app->run(); 
+#about page (static html )
+$app->get('/about', function() use($app) { 
+    return 'Hello '.$app['twig']->render('about.twig'); 
+}); 
 
+$app->run(); 
 ?>
