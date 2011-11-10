f0eead6b801fac4268900ce15ecd4f7f8ae7c1c3
zonoise <zonoise@gmail.com>
2011-11-11 01:35:46 +0900

    mod .htaccess

--- public_html/index.php
+++ public_html/index.php
@@ -3,12 +3,17 @@ require_once __DIR__.'/silex.phar';
 
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
 
 //Register Twig Extension
 $app->register(new Silex\Provider\TwigServiceProvider(),array(
-  'twig.path'           => __DIR__ . '/../views' ,
-  'twig.class_path' => __DIR__ .  '/../vender'
+  'twig.path'           => __DIR__.'/views' ,
+  'twig.class_path' => __DIR__.'/vender/Twig/lib'
 ));
+
+$app->get('/', function() use($app) { 
+  return 'top'; 
+}); 
 
 $app->get('/hello/{name}', function($name) use($app) { 
   return 'Hello '.$app->escape($name); 
@@ -16,12 +21,14 @@ $app->get('/hello/{name}', function($name) use($app) {
 
 #about page (static html )
 $app->get('/about', function() use($app) { 
-  return 'Hello '.$app['twig']->render('about.twig'); 
+  return  $app['twig']->render('about.twig'); 
 }); 
 
 
 $app->error(function (\Exception $e, $code) use ($app) {
-  return ('We are sorry, but something went terribly wrong.', $code);
+  return 'We are sorry, but something went terribly wrong.'
+  . $code
+   . '</pre>' . $app['twig']. '</pre>' ;
 });
 
 $app->run(); 
